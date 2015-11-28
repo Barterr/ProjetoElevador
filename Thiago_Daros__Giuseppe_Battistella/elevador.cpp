4185a75c29d2e2177ceb5f6fe1c65b1a1800ff05
kashfir <daros.thiago@gmail.com>
2015-11-28 14:39:52 -0200

    deltei o zip e deixei a pasta

---
+++ Thiago_Daros__Giuseppe_Battistella/elevador.cpp
@@ -0,0 +1,61 @@
+
+#include "elevador.h"
+
+void Elevador::inicializa(int totalAndares, int capacidade){
+	Elevador::totalAndares = totalAndares;
+	Elevador::capacidade = capacidade;
+	presentes = 0;
+	andarAtual = 0;
+}
+
+bool Elevador::entra(){
+	bool erro = false;
+	if (presentes < capacidade){
+		presentes++;
+		erro = true;
+		}
+	return erro;
+}
+
+bool Elevador::sai(){
+	bool erro = false;
+	if (presentes > 0){
+		 presentes--;
+		 erro = true;
+	}
+	return erro;
+}
+
+bool Elevador::sobe(){
+	bool erro = false;
+	if (andarAtual<totalAndares){
+		andarAtual++;
+		erro = true;
+	}
+	return erro;
+}
+
+bool Elevador::desce(){
+	bool erro = false;
+	if (andarAtual>0){
+		andarAtual--;
+		erro = true;
+	}
+	return erro;
+}
+
+int Elevador::getAndarAtual(){
+	return andarAtual;
+}
+
+int Elevador::getTotalAndares(){
+	return totalAndares;
+}
+
+int Elevador::getCapacidade(){
+	return capacidade;
+}
+
+int Elevador::getPresentes(){
+	return presentes;
+}
---
+++ Thiago_Daros__Giuseppe_Battistella/main.cpp
@@ -0,0 +1,82 @@
+#include "elevador.h"
+
+int main (){
+
+	int andares;
+	int capacidade;
+
+	bool on = true;
+	int option;
+
+	Elevador elev;
+
+	cout << "Entre com o total de andares:" << endl;
+	cin >> andares;
+	cout << "Entre com a capacidade do elevador:" << endl;
+	cin >> capacidade;
+
+	elev.inicializa(andares,capacidade);
+
+	cout << "1:sobe\n 2:desce\n 3:entra\n 4:sai\n 5:capacidade total\n 6:presentes\n 7:andar atual\n 8:total andares\n 9:sair" << endl;
+
+ 	while(on) {
+ 		cin >> option;
+ 		switch (option){
+ 			case 1:
+ 			if (elev.sobe()) {
+ 				cout << "Andar atual: " << elev.getAndarAtual()<<endl;
+ 			}
+ 			else {
+ 				cout << "O elevador se encontra no topo, andar: " << elev.getAndarAtual()<<endl;
+ 			}
+ 			break;
+
+ 			case 2:
+ 			if (elev.desce()) {
+ 				cout << "Andar atual: " << elev.getAndarAtual()<<endl;
+ 			}
+ 			else {
+ 				cout << "O elevador se encontra no terreo, andar: " << elev.getAndarAtual()<<endl;
+ 			}
+ 			break;
+
+ 			case 3:
+ 			if (elev.entra()) {
+ 				cout << "Presentes: " << elev.getPresentes()<<endl;
+ 			}
+ 			else {
+ 				cout << "O elevador se encontra cheio, presentes: " << elev.getPresentes()<<endl;
+ 			}
+ 			break;
+
+ 			case 4:
+ 			if (elev.sai()) {
+ 				cout << "Presentes: " << elev.getPresentes()<<endl;
+ 			}
+ 			else {
+ 				cout << "O elevador se encontra vazio, presentes: " << elev.getPresentes()<<endl;
+ 			}
+ 			break;
+
+ 			case 5:
+ 			cout << "Capacidade Total: " << elev.getCapacidade()<<endl;
+ 			break;
+
+ 			case 6:
+ 			cout << "Presentes: " << elev.getPresentes()<<endl;
+ 			break;
+
+ 			case 7:
+ 			cout << "Andar Atual: " << elev.getAndarAtual()<<endl;
+ 			break;
+
+ 			case 8:
+ 			cout << "Total de Andares: " << elev.getTotalAndares()<<endl;
+ 			break;
+
+ 			on = false;
+ 			case 9:
+ 			break;
+ 			}
+ 		}
+ 	}
