cba88eea4bbec9b19b03df5d79462d089bf309a4
Giuseppe Battistella <bartgb00@gmail.com>
2015-11-28 20:43:04 -0200

    aa

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif // win32
+
 #include <iostream>
-#include <chrono>
-#include <thread>
+#include <unistd.h>
 #include "botao.cpp"
 #include "porta.cpp"
+
 using namespace std;
+
+void sleepcp(int milliseconds){
+    #ifdef WIN32
+    Sleep(milliseconds);
+    #else
+    usleep(milliseconds * 1000);
+    #endif // win32
+}
 
 int main (){
   Porta porta;
   cout << "Aberta: " << porta.estaAberta() << endl;
   cout << "Sensor: " << porta.getSensor() << endl;
+  sleepcp(3*1000);
   porta.setSensor(1);
   porta.abrir();
   cout << "Aberta: " << porta.estaAberta() << endl;
